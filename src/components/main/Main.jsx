e954c7d256dd45bf2358175d9ff751fa85cb436e
Behruz <Behruz>
2025-01-19 19:32:52 +0300

    salom

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,19 +1,57 @@
 
 import React from 'react'
 import Skitka from './skitka/Skitka'
-
+import "../main/main.scss"
+import Main_and from './main_and/Main_and'
 const Main = () => {
   return (
     <>
       <div className="main">
         <div className="container">
-            <div className="main_skitka">
-                <Skitka/>
+          <div className="main_skitka">
+            <Skitka />
+          </div>
+          <div className="main_box">
+            <div className="big_cards">
+              <h2 className="main_box-title">
+                Наши услуги
+              </h2>
+              <div className="main_box-cards">
+                <div className="main_card-1">
+                  <img src="/public/8.png" alt="" />
+                  <h2 className="card_title1">
+                    Доставляем авто в любую точку Москвы
+                  </h2>
+                </div>
+                <div className="main_card-1">
+                  <img src="/public/9.png" alt="" />
+                  <h2 className="card_title2">
+                    Аренда авто с личным водителем
+                  </h2>
+                </div>
+                <div className="main_card-1">
+                  <img src="/public/10.png" alt="" />
+                  <h2 className="card_title3">
+                    Водитель на подмену
+                  </h2>
+                </div>
+              </div>
+            <h2 className="zakazat">
+            6 причин заказат аренду авто в  DizRental
+            </h2>
+            <div className="zakazat_card_box">
+              <div className="zakazat_card1">
+                <div className="card_small">
+                  <img src="/public/14.png" alt="" className="small_img" />
+                </div>
+              </div>
             </div>
-            <div className="main_box">
-                
             </div>
+          </div>
         </div>
+        <div className="main_and">
+                <Main_and />
+              </div>
       </div>
     </>
   )
---
+++ src/components/main/main_and/Main_and.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import '../main_and/mainAnd.scss'
+const Main_and = () => {
+  return (
+    <>
+      <div className="main_and">
+        <div className="container">
+          <div className="main_and_box">
+            <div className="main_and_card1">
+              <div className="card_esse">
+                <h2 className="card_title">
+                  Car hire in Dubai for different budget
+                </h2>
+                <p className="card_text1">
+                  We guarantee that you get technically
+                  sound, clean car with a full tank,
+                  comfortable interior. Our staff
+                  will independently pick up the
+                  car at the expiration of the rental
+                  term at the specified location.
+                </p>
+                <p className="card_text2">
+                  We strive to make car rental in Dubai
+                  affordable for everyone. That's why you
+                  can rent cars in our office:
+                  Economy. Sedans and SUVs you can
+                  rent from 149 AED ($40) per day.Comfort.
+                  Sports cars, vans, SUVs, sedans with
+                  comfortable saloon can be rented from
+                  219 AED ($59) per day.Premium. Premium
+                  SUVs and prestigious convertibles can be
+                  rent from AED 439 ($119) per day.
+                </p>
+              </div>
+              <div className="card_img">
+                <img src="/public/12.png" alt="" />
+              </div>
+            </div>
+            <div className="main_and_card2">
+              <div className="card_img">
+                <img src="/public/13.png" alt="" />
+              </div>
+              <div className="card_esse">
+                <h2 className="card2_title">
+                  Аренда машин в Москве
+                </h2>
+                <p className="card2_text1">
+                  Для получения более подробной информации
+                  о прокате авто можно посетить наш офис
+                  , оставить заявку на сайте или позвонить
+                  по телефону 7 (499) 110-20-47
+                </p>
+                <p className="card2_text2">
+                  Мы всегда рады помочь Вам по любым
+                   вопросам и обеспечить качественным авто.
+                </p>
+                
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main_and
